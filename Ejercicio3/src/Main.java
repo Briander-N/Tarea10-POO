dcef0e01da6468836ea74aadc80d2d026b6bd033
Briander <pochitobriander@gmail.com>
2026-05-12 17:33:14 -0500

    Ejercicio 3 con menu implementado terminado

--- Ejercicio3/src/Main.java
+++ Ejercicio3/src/Main.java
@@ -1,21 +1,51 @@
+import java.util.Scanner;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
-    Cajero c = new Cajero();
-    Administrador a = new Administrador();
-    Supervisor s = new Supervisor();
+    Scanner sc = new Scanner(System.in);
 
-    c.iniciarSesion("cajero", "123");
-    a.gestionarDatos();
+    int opcion = 0;
+    while (opcion != 4){
+        System.out.println("SISTEMA DE INICIO DE SESION");
+        System.out.println("Elige un rol: ");
+        System.out.println("1. Cajero");
+        System.out.println("2. Admin");
+        System.out.println("3. Supervisor");
+        System.out.println("4. Salir");
 
-    System.out.println();
+        opcion = sc.nextInt();
 
-    a.iniciarSesion("admin", "admin123");
-    a.generarReporte();
-    a.gestionarDatos();
+        switch (opcion){
+            case 1:
+                Cajero c = new Cajero();
+                c.iniciarSesion("cajero", "123");
+                break;
 
-    System.out.println();
+            case 2:
+                Administrador a = new Administrador();
 
-    s.iniciarSesion("supervisor", "sup123");
-    s.generarReporte();
+                a.iniciarSesion("admin", "admin123");
+                a.generarReporte();
+                a.gestionarDatos();
+                break;
+
+            case 3:
+                Supervisor s = new Supervisor();
+                s.iniciarSesion("supervisor", "sup123");
+                s.generarReporte();
+                break;
+
+            case 4:
+                System.out.println("Saliendo...");
+                break;
+
+            default:
+                System.out.println("Opcion invalida, intente nuevamente!!");
+
+
+        }
+    }
 }
+
+
+
